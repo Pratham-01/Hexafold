a15a9e2207eb16903bcd5da7ada7c153743d7efb
Pratham-01 <prathamjajodia1@gmail.com>
2022-07-27 17:52:06 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -1,4 +1,19 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 const app = express();
-app.get('user');
+var mongoclient = require("mongodb").MongoClient;
+var url = "mongodb+srv://hexafold:czZ7r2WUsoAcNZ3y@cluster0.jcb819v.mongodb.net/?retryWrites=true&w=majority";
+
+app.get("/",  (req, res) => {
+    mongoclient.connect(url, function(err, db) {
+        if (err) throw err;
+    
+        var dbo = db.db("hexafold");
+        dbo.collection("user").find({email:'prathamjajodia1@gmail.com'}).toArray( (err, result) => {
+            if (err) throw err;
+            res.send(result);
+        });
+    }) 
+});
+
+server = app.listen(8080);
