c39fa61d1536b571022d826cabcb3b7c831da5ec
Pratham-01 <prathamjajodia1@gmail.com>
2022-08-05 05:24:25 +0530

    Update getUserProjects.js

--- Server/api/project/userProjects/getUserProjects.js
+++ Server/api/project/userProjects/getUserProjects.js
@@ -3,7 +3,7 @@ var ObjectId = require('mongodb').ObjectId;
 
 exports.getUserProjects = async (req, res) => {
 	console.log('Request received for getting projects of a user');
-	var id = req.params.userId;
+	var email = req.params.userId;
 	constants.mongoclient.connect(constants.url, function (err, db) {
 		if (err) {
 			res.status(500).send({ errors: err });
@@ -12,7 +12,7 @@ exports.getUserProjects = async (req, res) => {
 		var dbo = db.db('hexafold');
 		dbo
 			.collection('project')
-			.find({ user: new ObjectId(id) })
+			.find({ users: email })
 			.toArray((err, result) => {
 				if (err) {
 					res.status(500).send({ errors: err });
