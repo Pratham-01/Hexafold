61c50d44034a390acba43ca427194b1a147b3367
divyasharma5 <divyasharma512d@gmail.com>
2022-08-05 01:17:21 +0530

    create client create project done

--- Server/api/client/createClient.js
+++ Server/api/client/createClient.js
@@ -7,18 +7,30 @@ exports.createClient = async (req, res) => {
 		var post = {
 			email: req.body.email,
 			password: req.body.password,
+			company: req.body.company ? req.body.company : '',
+			company_id: req.body.company_id,
 		};
 
 		constants.mongoclient.connect(constants.url, function (err, db) {
 			if (err) throw err;
 
 			var dbo = db.db('hexafold');
-			dbo.collection('client').insertOne(post, function (err, result) {
-				if (err) throw err;
-				console.log('New Client Added', result);
-				db.close();
-				res.status(200).send({ message: 'New Client Added' });
-			});
+
+			dbo
+				.collection('client')
+				.find({ email: post.email })
+				.toArray((err, result) => {
+					if (result.length != 0) {
+						res.status(200).send({ message: 'Client already exists' });
+					} else {
+						dbo.collection('client').insertOne(post, function (err, result) {
+							if (err) throw err;
+							console.log('New Client Added', result);
+							db.close();
+							res.status(200).send({ message: 'New Client Added' });
+						});
+					}
+				});
 		});
 	} catch (err) {
 		res.status(500).send({ errors: err });
--- Server/api/project/userProjects/createProject.js
+++ Server/api/project/userProjects/createProject.js
@@ -6,7 +6,7 @@ exports.createProject = async (req, res) => {
 		console.log('Request received for creating a project');
 		var post = {
 			clients: req.body.clients ? req.body.client : '',
-			title: req.body.title ? req.body.title : '',
+			projectTitle: req.body.projectTitle ? req.body.projectTitle : '',
 			comments: req.body.comments ? req.body.comments : '',
 			features: req.body.features ? req.body.features : '',
 			user: req.body.user ? req.body.user : '',
@@ -17,12 +17,21 @@ exports.createProject = async (req, res) => {
 			if (err) throw err;
 
 			var dbo = db.db('hexafold');
-			dbo.collection('project').insertOne(post, function (err, result) {
-				if (err) throw err;
-				console.log('New Project Added', result);
-				res.status(200).send({ message: 'New Project Added' });
-				db.close();
-			});
+			dbo
+				.collection('project')
+				.find({ projectTitle: post.projectTitle })
+				.toArray((err, result) => {
+					if (result.length != 0) {
+						res.status(200).send({ message: 'Project already exists' });
+					} else {
+						dbo.collection('project').insertOne(post, function (err, result) {
+							if (err) throw err;
+							console.log('New Project Added', result);
+							res.status(200).send({ message: 'New Project Added' });
+							db.close();
+						});
+					}
+				});
 		});
 	} catch (err) {
 		res.status(500).send({ errors: err });
