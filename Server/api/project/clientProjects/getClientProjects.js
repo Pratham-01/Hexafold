0751838b074b0f534a5863bb3066c6f60d60c9f7
divyasharma5 <divyasharma512d@gmail.com>
2022-08-06 22:23:53 +0530

    api

--- Server/api/project/clientProjects/getClientProjects.js
+++ Server/api/project/clientProjects/getClientProjects.js
@@ -2,23 +2,22 @@ var constants = require('../../../constants/constantVariables');
 var ObjectId = require('mongodb').ObjectId;
 exports.getClientProjects = async (req, res) => {
 	try {
-		console.log('Request received for getting projects of a client/user');
-		var id = new ObjectId(req.params.clientId);
-		console.log('id=', id);
+		console.log('Request received for getting projects of a client');
+		var clientId = req.params.clientId;
 		constants.mongoclient.connect(constants.url, function (err, db) {
 			if (err) {
 				res.status(500).send({ errors: err });
 				return;
-			};
+			}
 			var dbo = db.db('hexafold');
 			dbo
 				.collection('project')
-				.find({ clients: id })
+				.find({ clients: { $in: [clientId] } })
 				.toArray((err, result) => {
 					if (err) {
 						res.status(500).send({ errors: err });
 						return;
-					};
+					}
 					console.log(result);
 					db.close();
 					res.send(result);
