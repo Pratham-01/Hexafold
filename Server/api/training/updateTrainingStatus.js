a66464b064937f3294a5380bc10d582530fe17b1
Pratham-01 <prathamjajodia1@gmail.com>
2022-08-07 19:45:32 +0530

    training complete bug fixed

--- Server/api/training/updateTrainingStatus.js
+++ Server/api/training/updateTrainingStatus.js
@@ -4,7 +4,7 @@ var ObjectId = require('mongodb').ObjectId;
 exports.updateTrainingStatus = async (req, res) => {
 	try {
 		console.log('Request received for updating Training Status');
-        var training_id = req.body.training_id;
+        var training_id = new ObjectId(req.body.training_id);
         var user = req.body.user;
         var status = req.body.status;
 		
@@ -14,21 +14,37 @@ exports.updateTrainingStatus = async (req, res) => {
 				return;
 			};
 
-            var myquery = {email: user};
-            var newvalues = {$set: { 'trainings.$[ele].status': status }};
-            var arrayFilters = { arrayFilters: [{ 'ele.training_id': new ObjectId(training_id) }] };
-
 			var dbo = db.db('hexafold');
+
 			dbo
-				.collection('user')
-				.updateOne(myquery, newvalues, arrayFilters, function(err, result) {
-					if (err) {
+				.collection('training')
+                .find({ _id: training_id })
+                .toArray((err, result) => {
+                    if (err) {
 						res.status(500).send({ errors: err });
 						return;
-					};
-					db.close();
-					res.status(200).send({message: 'Training Status Updated Successfully'})
-				});
+					};	
+			    
+					var points = result[0].reward;
+
+					var myquery = {email: user};
+					var newvalues = {$set: { 'trainings.$[ele].status': status }, $inc : {reward_points: points}};
+					var arrayFilters = { arrayFilters: [{ 'ele.training_id': training_id }] };
+
+					
+					dbo
+						.collection('user')
+						.updateOne(myquery, newvalues, arrayFilters, function(err, result) {
+							if (err) {
+								res.status(500).send({ errors: err });
+								return;
+							};
+							db.close();
+							res.status(200).send({message: 'Training Status Updated Successfully'})
+						});
+                });
+
+            
 		});
 	} catch (err) {
 		res.status(500).send({errors: err});
